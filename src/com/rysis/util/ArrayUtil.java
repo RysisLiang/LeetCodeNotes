1fe2804d431f7d9c086e2f254209132190105d06
Rysis <liang1599515@163.com>
2021-01-26 23:38:08 +0800

    [c]增加嵌套数组的方法

--- src/com/rysis/util/ArrayUtil.java
+++ src/com/rysis/util/ArrayUtil.java
@@ -1,6 +1,8 @@
 package com.rysis.util;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * ArrayUtil
@@ -22,5 +24,23 @@ public class ArrayUtil {
                 .filter(s -> !"".equals(s))
                 .mapToInt(Integer::parseInt)
                 .toArray();
+    }
+
+    /**
+     * 转嵌套数组
+     *
+     * @param str "[[1,3],[5,4],[7,2]]"
+     * @return
+     */
+    public static int[][] handleToNestedArray(String str) {
+        List<int[]> collect = Arrays.stream(str.substring(2, str.length() - 2).split("],\\["))
+                .filter(s -> !"".equals(s))
+                .map(s -> handleToArray("[" + s + "]"))
+                .collect(Collectors.toList());
+        int[][] ints = new int[collect.size()][2];
+        for (int i = 0; i < ints.length; i++) {
+            ints[i] = collect.get(i);
+        }
+        return ints;
     }
 }
