210fea4fea2c27deed0fbfc6c43bec5aa07398eb
liangxiao <liang1599515@163.com>
2021-02-27 23:54:15 +0800

    [c]130

--- src/com/rysis/util/ArrayUtil.java
+++ src/com/rysis/util/ArrayUtil.java
@@ -39,6 +39,24 @@ public class ArrayUtil {
     }
 
     /**
+     * 转数组
+     *
+     * @param str "["tars","rats","arts","star"]"
+     * @return
+     */
+    public static char[] handleToCharArray(String str) {
+        List<Character> collect = Arrays.stream(str.substring(1, str.length() - 1).split(","))
+                .filter(StringUtil::isDefined)
+                .map(s -> s.charAt(1))
+                .collect(Collectors.toList());
+        char[] result = new char[collect.size()];
+        for (int i = 0; i < collect.size(); i++) {
+            result[i] = collect.get(i);
+        }
+        return result;
+    }
+
+    /**
      * 转嵌套数组
      *
      * @param str "[[1,3],[5,4],[7,2]]"
@@ -50,6 +68,24 @@ public class ArrayUtil {
                 .map(s -> handleToIntArray("[" + s + "]"))
                 .collect(Collectors.toList());
         int[][] ints = new int[collect.size()][2];
+        for (int i = 0; i < ints.length; i++) {
+            ints[i] = collect.get(i);
+        }
+        return ints;
+    }
+
+    /**
+     * 转嵌套数组
+     *
+     * @param str "[[1,3],[5,4],[7,2]]"
+     * @return
+     */
+    public static char[][] handleToNestedCharArray(String str) {
+        List<char[]> collect = Arrays.stream(str.substring(2, str.length() - 2).split("],\\["))
+                .filter(StringUtil::isDefined)
+                .map(s -> handleToCharArray("[" + s + "]"))
+                .collect(Collectors.toList());
+        char[][] ints = new char[collect.size()][2];
         for (int i = 0; i < ints.length; i++) {
             ints[i] = collect.get(i);
         }
