0fe9a98719aadd67b6be0d348ca3403b4dabd866
liangxiao <liang1599515@163.com>
2021-06-15 00:15:19 +0800

    [c]456 暴力法

--- src/com/rysis/queue/Main_456.java
+++ src/com/rysis/queue/Main_456.java
@@ -23,13 +23,7 @@ public class Main_456 {
         System.out.println(find132pattern(ArrayUtil.handleToIntArray(s)));
     }
 
-    // 单调栈 132模式其实，只要找出最小的1和最大的3，那么有2满足就可以了。
-    // 单调栈的话，里面的元素都是递增或者递减的。这里我们需要找出最小的1和最大的3。
-    // 132里面可以拆成两部分，一部分用单调栈来维护，另一部分与栈去比较即可。
-    // 13是栈的话，2不好与栈中比较。32是栈的话，1只要比2小即可。所以我们可以用栈去维护32的，然后可以从右往左去遍历，这样也能维护了角标的顺序。
-    // 只要元素比栈顶大时，就弹栈，并记录下来当做2（满足角标关系，也满足32的大小关系），然后把元素压入栈。
-    // 如果比栈顶小时，先与2元素比较。不符合则直接压入栈。
-    public static boolean find132pattern(int[] nums) {
+    public static boolean find132pattern2(int[] nums) {
         if (nums.length < 3) {
             return false;
         }
@@ -54,6 +48,28 @@ public class Main_456 {
             linkedList.push(num);
         }
 
+        return false;
+    }
+
+    // 暴力迭代。会超时
+    public static boolean find132pattern(int[] nums) {
+        if (nums.length < 3) {
+            return false;
+        }
+        // 1
+        int m1 = nums[0];
+
+        // 找出3
+        for (int i = 1, len = nums.length; i < len; i++) {
+            // 找出2
+            for (int j = len - 1; j > i; j--) {
+                if (m1 < nums[j] && nums[j] < nums[i]) {
+                    return true;
+                }
+            }
+            // 保持m1一直是3的左侧最小值
+            m1 = Math.min(m1, nums[i]);
+        }
         return false;
     }
 
